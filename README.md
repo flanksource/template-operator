1f1c0466677009eb2f18fd50766a2752757b1039
Moshe Immerman <moshe.immerman@gmail.com>
2020-09-11 11:10:18 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
 # template-operator
+
+
+
+
+An example replicating the functionality of https://github.com/pusher/quack
+
+```yaml
+apiVersion: templating.flanksource.com/v1
+kind: Template
+metadata:
+  name: dynamic-ingress-hostnames
+spec:
+  onceoff: true # need to set this flag as otherwise it will trigger an endless loop appending the domain to iteself.
+  namespaceSelector:
+     quack.pusher.com/enabled: "true"
+  objectSelector:
+    - apiVersion: extensions/v1beta1
+      kind: Ingress
+  patches:
+    - spec:
+        rules:
+            # append a dynamic domain specified in a configmap to the domain specified in the ingress
+          - host: "{{.source.spec.rules[0].host}}.{{-   kget "cm/quack/quack-config" "data.domain" -}}"
+            tls:
+              hosts:
+                -  "{{.source.spec.rules[0].host}}.{{-   kget "cm/quack/quack-config" "data.domain" }}"
+```
+
+An example replicating  https://github.com/redhat-cop/namespace-configuration-operator
+```yaml
+apiVersion: templating.flanksource.com/v1
+kind: Template
+metadata:
+  name: dcp-on-demand
+  namespace: kube-system
+spec:
+  labelSelector:
+    matchLabels:
+      owner: dh-dcp
+  resources:
+    - kind: RoleBinding
+      apiVersion: rbac.authorization.k8s.io/v1
+      metadata:
+        name: creator
+      subjects:
+        - kind: Group
+          name: Owner
+          apiGroup: rbac.authorization.k8s.io
+      roleRef:
+        apiGroup: rbac.authorization.k8s.io
+        kind: ClusterRole
+        name: namespace-admin
+```
---
+++ api/v1/template_types.go
@@ -0,0 +1,89 @@
+/*
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	"encoding/json"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// TemplateSpec defines the desired state of Template
+type TemplateSpec struct {
+
+	// Source selects objects on which to use as a templating object
+	Source ResourceSelector `json:"source,omitempty"`
+
+	// Target optionally allows to lookup related resources to patch, defaults
+	// to the source object selected
+	// +optional
+	PatchTarget ResourceSelector `json:"patchTarget,omitempty"`
+
+	// Resources is a list of new resources to create for each source objected found
+	// Must specify at least resources or patches or both
+	// +optional
+	Resources []json.RawMessage `json:"resources,omitempty"`
+
+	// Patches ia list of strategic merge patches to apply to to the targets
+	// Must specify at least resources or patches or both
+	// +optional
+	Patches []json.RawMessage `json:"patches,omitempty"`
+
+	// Onceoff will not apply templating more than once (usually at admission stage)
+	Onceoff bool `json:"onceoff,omitempty"`
+}
+
+// TemplateStatus defines the observed state of Template
+type TemplateStatus struct {
+}
+
+type ResourceSelector struct {
+	LabelSelector      metav1.LabelSelector `json:"labelSelector,omitempty"`
+	NamespaceSelector  metav1.LabelSelector `json:"namespaceSelector,omitempty"`
+	AnnotationSelector map[string]string    `json:"annotationSelector,omitempty"`
+	FieldSelector      string               `json:"fieldSelector,omitempty"`
+	ObjectSelector     []ObjectSelector     `json:"object,omitempty"`
+}
+
+type ObjectSelector struct {
+	Kind       string `json:"kind,omitempty"`
+	APIVersion string `json:"apiVersion,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// Template is the Schema for the templates API
+type Template struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   TemplateSpec   `json:"spec,omitempty"`
+	Status TemplateStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// TemplateList contains a list of Template
+type TemplateList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Template `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Template{}, &TemplateList{})
+}
