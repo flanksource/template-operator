bffea4fac2c58cde3148807b17ab567b23aa3ad5
Teodor Pripoae <teodor.pripoae@gmail.com>
2020-11-24 14:08:38 +0200

    Use latest karina version for e2e

--- test/e2e.sh
+++ test/e2e.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-export KARINA_VERSION=v0.20.4
+export KARINA_VERSION=v0.21.0
 export KARINA="./karina -c test/config.yaml"
 export KUBECONFIG=~/.kube/config
 export DOCKER_API_VERSION=1.39
